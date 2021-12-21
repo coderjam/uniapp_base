da70aeae0120d641c0aeb3b7213257cd44fc01a2
邓健 <JDeng@senparc.com>
2021-12-21 10:31:12 +0800

    add utils.js

--- uniapp_uview/common/http.api.js
+++ uniapp_uview/common/http.api.js
@@ -1,17 +1,14 @@
 //api集合
 let apiApp = {
 	login: '/api/login',
-	getInfo: '/777/777/index',
+	getInfo: '/api/777/index',
+	hotSearchUrl: '/api/77/hot_search'
 }
-let apiHome = {
-	hotSearchUrl: '/777/77/hot_search'
-}
-
 // 此处第二个参数vm，就是我们在页面使用的this，你可以通过vm获取vuex等操作
 const install = (Vue, vm) => {
 
 	//api方法
-	let getSearch = (params = {}) => vm.$u.get(apiHome.hotSearchUrl, params);
+	let getSearch = (params = {}) => vm.$u.get(apiApp.hotSearchUrl, params);
 	let getInfo = (params={}) => vm.$u.post(apiApp.getInfo, params);
 	
 	// 将各个定义的接口名称，统一放进对象挂载到vm.$u.api(因为vm就是this，也即this.$u.api)下
