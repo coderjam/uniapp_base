ef9a7335bdba68fc43b4263d294c9925f86e10f2
jam <jamis404@163.com>
2022-08-16 17:21:55 +0800

    分包配置

---
+++ my-project-vue2/src/pages/user/index.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<text class="title">{{ title }}</text>
+		</view>
+		<u-button>默认按钮</u-button>
+		<u-button type="primary">主要按钮</u-button>
+		<u-button type="success">成功按钮</u-button>
+		<u-button type="info">信息按钮</u-button>
+		<u-button type="warning">警告按钮</u-button>
+		<u-button type="error">危险按钮</u-button>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: 'Hello lanba jam!'
+		}
+	},
+	onLoad() {
+
+	},
+	methods: {
+
+	}
+}
+</script>
+
+<style>
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+
+.logo {
+	height: 200rpx;
+	width: 200rpx;
+	margin: 200rpx auto 50rpx auto;
+}
+
+.text-area {
+	display: flex;
+	justify-content: center;
+}
+
+.title {
+	font-size: 36rpx;
+	color: #8f8f94;
+}
+</style>
