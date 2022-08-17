a2776fc48e19abf2f1040206cd8e3b71e5aa1a15
jam <jamis404@163.com>
2022-08-17 14:01:51 +0800

    自定义tabbar

--- my-project-vue2/src/pages/home/index.vue
+++ my-project-vue2/src/pages/home/index.vue
@@ -11,6 +11,9 @@
 		<u-button type="info">信息按钮</u-button>
 		<u-button type="warning">警告按钮</u-button>
 		<u-button type="error">危险按钮</u-button>
+
+		<!-- 自定义tabbar -->
+		<!-- <u-tabbar :list="vuex_tabbar" :mid-button="true"></u-tabbar> -->
 	</view>
 </template>
 
